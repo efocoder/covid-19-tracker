8f83da2bfca0e23ed70a6a469bac4b2d16690986
efocoder <clemsedem@gmail.com>
2022-01-09 20:48:42 +0000

    intial commit

---
+++ src/components/Header.vue
@@ -0,0 +1,11 @@
+<template>
+  <header class="text-center text-white bg-blue-800 p-4 mb-10">
+    <div class="text-3xl md:text-5xl font-bold"><i class="fa fa-viruses"></i> Covid-19 Tracker</div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Headeer',
+}
+</script>
